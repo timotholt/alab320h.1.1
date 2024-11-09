7089504d8093807adf6714067d475def8989a46d
Timothy Otholt <timotholt@gmail.com>
2024-11-08 22:58:44 -0800

    workingon right side

--- src/components/EmployeePage.jsx
+++ src/components/EmployeePage.jsx
@@ -1,7 +1,7 @@
 
 import { Header } from './Header';
 
-export function EmployeePage(employeeList) {
+export function EmployeePage(employee) {
 
     const style = {
         border: "0.1rem solid gold",
@@ -12,6 +12,34 @@ export function EmployeePage(employeeList) {
     return (
         <div style={style}>
             <Header title="Employee"/>
+            <div style={{
+      border: '0.1rem solid red',
+      margin: '0.2rem',
+      padding: '0.3rem',
+      display: 'flex',
+      alignItems: 'center'
+    }}>
+      <div style={{
+        padding: '0.2rem',
+        flex: '0 0 auto',
+        width: '40px' // Adjust width as needed
+      }}>
+        <img src="public\icon-5359553_1280.webp" alt="Avatar" width="40px" height="40px" />
+      </div>
+      <div style={{
+        flex: 1,
+        fontSize: '0.8rem',
+        fontWeight: 'normal',
+        color: 'black',
+        textAlign: 'left',
+        marginTop: '-0.6rem',
+        marginLeft: '0.2rem'
+      }}>
+        <div><strong>{employee.name}</strong></div>
+        <div style={{ fontSize: '0.7rem', color: '#777' }}>
+            {employee.title}</div>
+      </div>
+    </div>
         </div>
     )
 }
